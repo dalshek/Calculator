b3c071f2bcc921f16abf7fac25f0dc239f069c0a
dalshek <cdiezpag@gmail.com>
2023-06-10 10:09:42 +0200

    Display and basic button listener done

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@
 let firstNum=0;
 let secondNum=0;
 let operator='';
+const display = document.querySelector('#display');
+let displayNumber = 0;
 
 //------------OPERATION FUNCTIONS--------------//
 let add = (num1, num2) => num1 + num2;
@@ -15,5 +17,24 @@ let operate = (firstNum, secondNum, operator) => {
     else if(operator == '-') return substract(firstNum, secondNum);
     else if(operator == '*') return multiply(firstNum, secondNum);
     else if(operator == '/') return divide(firstNum, secondNum);
-
 }
+
+//----------BUTTON LISTENER FUNCTION----------//
+/*let buttonListener = ("click", () => {
+    
+    whichButton.forEach((boton) => {
+        console.log(boton.textContent);
+    });
+});*/
+
+const buttonListener = () => {
+    const whichButton = document.querySelectorAll('button');
+    whichButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        display.textContent+=(button.textContent);
+        displayNumber = display.textContent;
+    });
+});
+}
+
+buttonListener();
